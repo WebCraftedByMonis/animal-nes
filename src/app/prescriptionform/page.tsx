d9210b51fe1092b57484f42ca2cc9a72c1df5737
Monis Raza <monisrazadev@gmail.com>
2025-08-20 12:30:45 +0500

    S

--- src/app/prescriptionform/page.tsx
+++ src/app/prescriptionform/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 'use client'
+export const dynamic = 'force-dynamic';
 
 import { useEffect, useState } from 'react'
 import { useForm, useFieldArray } from 'react-hook-form'
