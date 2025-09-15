80e3ea0d507afd96ec867b6fcbc17f066e23d9c8
Monis Raza <monisrazadev@gmail.com>
2025-09-15 13:35:54 +0500

    added cookies safe parsing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,17 @@ import type { NextAuthConfig } from "next-auth";
 export const authConfig = {
   adapter: PrismaAdapter(prisma),
   trustHost: true,
+  cookies: {
+    pkceCodeVerifier: {
+      name: `next-auth.pkce.code_verifier`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true
+      }
+    }
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
