b51ad217fb1415c162f07296982bfb7f4ac492c6
Monis Raza <monisrazadev@gmail.com>
2025-09-15 13:21:12 +0500

    added trusthost true

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
